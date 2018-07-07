65fcabbc8ad77188565055766bc60635fff2ad76
findly <15250289553@163.com>
2018-07-07 22:59:31 +0800

    加入创建数据库和表的各种配置

--- app/src/main/java/com/example/admin/coolweather/db/Province.java
+++ app/src/main/java/com/example/admin/coolweather/db/Province.java
@@ -3,4 +3,38 @@ package com.example.admin.coolweather.db;
 import org.litepal.crud.DataSupport;
 
 public class Province extends DataSupport {
+
+    private int id = 0;
+    private String provincename = "";
+    private int provicecode = 0;
+
+    public Province(int id,String provincename,int provicecode){
+        this.id = id;
+        this.provicecode = provicecode;
+        this.provincename = provincename;
+    }
+
+    public int getid(){
+        return id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public int getcode(){
+        return provicecode;
+    }
+
+    public void setcode(int code){
+        this.provicecode = code;
+    }
+
+    public String getprovincename(){
+        return this.provincename;
+    }
+
+    public void setProvincename(String provincename){
+        this.provincename = provincename;
+    }
 }
